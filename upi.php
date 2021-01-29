0c2bbd15b7cbd02ad9c17a59ff9876d6e5148f65
sanjeev shetty <sanjeevkse@gmail.com>
2021-01-29 08:35:45 +0530

    Update upi.php

--- upi.php
+++ upi.php
@@ -1,5 +1,8 @@
 <?php
 
-  header('Location: upi://pay?pa='.$_GET['upi_address'].'&cu=INR&appName=SetLUp&tn='.$_GET['upi_notes'].'&am='.$_GET['upi_amount']);
+  // header('Location: upi://pay?pa='.$_GET['upi_address'].'&cu=INR&appName=SetLUp&tn='.$_GET['upi_notes'].'&am='.$_GET['upi_amount']);
 
 ?>
+<script>
+  location.href = "upi://pay?pa=sanjeev@okaxis&cu=INR&tn=dafdf&am=20";
+</script>
